8974f5d8dc586ce43eb232d2ccc4f7a02e62354d
Joshua Abu <joshujah007@gmail.com>
2016-05-27 23:16:11 -0400

    updated node modules, made style changes

--- logic.js
+++ logic.js
@@ -6,17 +6,26 @@ myApp.controller('LandingPageCtrl', ['$scope', '$firebaseAuth', function($scope,
   $scope.signIn = function(){
     var firebaseObj = new Firebase("https://hos-login.firebaseio.com");
     $scope.authObj = $firebaseAuth(firebaseObj);
-      $scope.authObj.$authWithPassword({
-        "email": $scope.email,
-        "password": $scope.password
-        }, function(error, authData) {
-        if (error) {
-          console.log("Login Failed!", error);
-        } else {
-          console.log("Authenticated successfully with payload:", authData);
-        }
-      });
+    $scope.authObj.$authWithPassword({
+      "email": $scope.email,
+      "password": $scope.password
+      }, function(error, authData) {
+      if (error) {
+        alert("Login Failed!", error);
+      } else {
+        console.log("Authenticated successfully with payload:", authData);
+        $scope.email = "";
+        $scope.password = "";
+      }
+    });
   };
+  // $scope.gitSignIn = function(){
+  //     $scope.authObj.$authWithOAuthToken("github", "<ACCESS_TOKEN>").then(function(authData) {
+  //       console.log("Logged in as:", authData.uid);
+  //     }).catch(function(error) {
+  //       console.error("Authentication failed:", error);
+  //     });
+  // };
 
 
 
